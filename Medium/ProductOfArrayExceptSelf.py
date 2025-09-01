434de2f7e58ee5888b1fa8bf0bb7f45eb75c2f4d
Ankita Sharma <113629197+ankitashrm215@users.noreply.github.com>
2025-09-01 14:05:11 -0500

    use prefix and postfix for optimized approach (#16)

--- Medium/ProductOfArrayExceptSelf.py
+++ Medium/ProductOfArrayExceptSelf.py
@@ -11,7 +11,6 @@ Input: nums = [-1,0,1,2,3]
 Output: [0,-6,0,0,0]
 '''
 def productExceptSelf(nums):
-
     
     ''' This is the brute force approach
     Time complexity: O(n*n) where n is the length of nums
@@ -28,4 +27,25 @@ def productExceptSelf(nums):
             j = j + 1
         allProd.append(prod)
     return allProd
+
+def productExceptSelfOptimised(nums):
+
+    ''' This is tthe prefix/postfix approach
+    Time complexity: O(n) where n is the length of nums
+    Space complexity: O(1) res doesn't count in the complexity
+    '''
+    l = len(nums)
+        prefix = 1
+        res = []
+        for i in range(0, l):
+            res.append(prefix)
+            prefix = prefix * nums[i]
+        
+        postfix = 1
+        for j in range(l-1, -1, -1):
+            res[j] = res[j] * postfix
+            postfix = postfix * nums[j]
+        return res
+
 print(productExceptSelf([1,2,4,6]))
+print(productExceptSelfOptimised([1,2,4,6]))
